e6b6b1d00d21c6260dce3dcd5b97f4d87ced5ad4
AleZavRan <zavalaa333@gmail.com>
2025-02-06 14:15:59 -0600

    Sign Up page first styling ended.

--- obj/Debug/net8.0/WebNutrition.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebNutrition.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebNutrition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4cb67ac11363df4a543c6d866914f2b06b78825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999ed607387ccda91d8cd60c38ffc5b1c97372c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebNutrition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebNutrition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
